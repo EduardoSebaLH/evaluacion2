38234fb6ca5bf7276d17ca4c973f4f7a6dffe9ad
Eduardo Lizana <eduardosebalh@gmail.com>
2022-08-05 23:14:53 -0400

    primera carga

---
+++ database/migrations/2022_08_06_000411_create_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id_stock');
+            $table->integer('cantidad');
+            $table->integer('precio');
+            $table->string('estado');
+            $table->foreignId('sucursal_id')->constrained();
+            $table->foreignId('producto_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
